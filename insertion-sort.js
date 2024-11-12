aa034d4b5bd7190d926ad1f85f82898ff946f323
Abayomi Folarin Festus <maicharles001@gmail.com>
2024-11-12 19:15:34 +0100

    First commit
    First commit

---
+++ insertion-sort.js
@@ -0,0 +1,16 @@
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    let key = array[i];
+    let j = i - 1;
+
+    while (j >= 0 && array[j] > key) {
+      array[j + 1] = array[j];
+      j = j - 1;
+    }
+    array[j + 1] = key;
+  }
+  return array;
+}
+
+let arr = [12, 11, 13, 5, 6];
+console.log("Sorted Array:", insertionSort(arr));
